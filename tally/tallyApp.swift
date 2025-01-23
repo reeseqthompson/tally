126e927cafd5e3e0f8722289825dd0bfdc2b2c00
Reese Thompson <rqt3@cornell.edu>
2025-01-22 23:41:40 -0500

    Initial Commit

---
+++ tally/tallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tallyApp.swift
+//  tally
+//
+//  Created by Reese Thompson on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct tallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tallyTests/tallyTests.swift
@@ -0,0 +1,16 @@
+//
+//  tallyTests.swift
+//  tallyTests
+//
+//  Created by Reese Thompson on 1/22/25.
+//
+
+import Testing
+
+struct tallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
